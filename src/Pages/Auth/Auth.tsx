a62a1494a6a18368fea8021ba01c9f9cc4b3efd8
NataliaStadnik <128145143+NataliaStadnik@users.noreply.github.com>
2025-02-26 23:35:03 +0300

    add auth-layout

--- src/Pages/Auth/Auth.tsx
+++ src/Pages/Auth/Auth.tsx
@@ -1,20 +1,63 @@
-import {Link, useLocation} from 'react-router-dom'
 import LoginPage from './ui/LoginPage'
 import RegisterPage from './ui/RegisterPage'
+import {FC} from 'react'
+import {Box, styled} from '@mui/material'
 
-const Auth = () => {
-  const location = useLocation().pathname
+import {StyledLinkList} from '@/Widgets'
+
+interface AuthProps {
+  location: 'login' | 'signup'
+}
+
+const Container = styled(Box)(({theme}) => ({
+  maxWidth: '1440px',
+  height: '100vh',
+  margin: '0 auto',
+  padding: '0 20px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  backgroundColor: theme.palette.background.default,
+}))
+
+const Header = styled(Box)({
+  width: '100%',
+  paddingTop: '5px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+})
+
+const Content = styled(Box)({
+  width: '100%',
+  padding: '20px 0',
+})
+
+const Auth: FC<AuthProps> = ({location}) => {
+  const links = [
+    {
+      to: '/auth/login',
+      label: 'Log In',
+      active: location === 'login',
+    },
+    {
+      to: '/auth/signup',
+      label: 'Sign Up',
+      active: location === 'signup',
+    },
+  ]
 
   return (
-    <div className="">
-      <div className="">
-        <Link to="/auth/login">Log In</Link>
-        <Link to="/auth/signup">Sign Up</Link>
-      </div>
+    <Container>
+      <Header>
+        <StyledLinkList arrLinks={links} />
+      </Header>
 
-      {location === '/auth/login' && <LoginPage />}
-      {location === '/auth/signup' && <RegisterPage />}
-    </div>
+      <Content>
+        {location === 'login' && <LoginPage />}
+        {location === 'signup' && <RegisterPage />}
+      </Content>
+    </Container>
   )
 }
 
--- src/Pages/ui/index.ts
+++ src/Pages/ui/index.ts
@@ -1,1 +1,3 @@
-export {}
+export { default as Auth } from "../Auth/Auth";
+export { default as ForgotPassword } from "../ChangePassword/ui/ForgotPassword";
+export { default as ResetPassword } from "../ChangePassword/ui/ResetPassword";
