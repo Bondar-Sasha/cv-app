6ecbd55c795c6f066705813992f72bdea9106ee9
NataliaStadnik <128145143+NataliaStadnik@users.noreply.github.com>
2025-02-28 00:26:24 +0300

    update apollo settings

--- src/App/providers/ApolloClient.tsx
+++ src/App/providers/ApolloClient.tsx
@@ -1,8 +1,6 @@
 import {ApolloClient, InMemoryCache, HttpLink} from '@apollo/client'
 import {setContext} from '@apollo/client/link/context'
 import {onError} from '@apollo/client/link/error'
-import {TokenRefreshLink} from 'apollo-link-token-refresh'
-import {jwtDecode} from 'jwt-decode'
 
 const httpLink = new HttpLink({
   uri: 'https://cv-project-js.inno.ws/api/graphql',
@@ -23,47 +21,6 @@ const authLink = setContext(
   }
 )
 
-const isTokenValidOrUndefined = () => {
-  const token = localStorage.getItem('access_token')
-  if (!token) return true
-
-  try {
-    const {exp} = jwtDecode(token)
-    return Date.now() < exp * 1000
-  } catch {
-    return false
-  }
-}
-
-const refreshLink = new TokenRefreshLink({
-  accessTokenField: 'access_token',
-  isTokenValidOrUndefined,
-  fetchAccessToken: () => {
-    return fetch('https://cv-project-js.inno.ws/api/graphql', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        query: `
-          mutation updateToken {
-            updateToken {
-              access_token
-            }
-          }
-        `,
-      }),
-    }).then((response) => response.json())
-  },
-  handleFetch: (accessToken) => {
-    localStorage.setItem('access_token', accessToken)
-  },
-  handleError: (err) => {
-    console.warn('Your refresh token is invalid. Try to relogin')
-    console.error(err)
-  },
-})
-
 const errorLink = onError(({graphQLErrors, networkError, protocolErrors}) => {
   if (graphQLErrors)
     graphQLErrors.forEach(({message}) =>
@@ -82,7 +39,7 @@ const errorLink = onError(({graphQLErrors, networkError, protocolErrors}) => {
 })
 
 export const client = new ApolloClient({
-  link: authLink.concat(refreshLink).concat(httpLink).concat(errorLink),
+  link: authLink.concat(httpLink).concat(errorLink),
   headers: {
     'Content-Type': 'application/json',
   },
