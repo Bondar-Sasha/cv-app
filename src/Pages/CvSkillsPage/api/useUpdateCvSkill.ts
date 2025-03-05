045f7f7437570e00739783e85942e85eddf1003a
NataliaStadnik <128145143+NataliaStadnik@users.noreply.github.com>
2025-03-05 19:31:15 +0300

    add request for cv skills

---
+++ src/Pages/CvSkillsPage/api/useUpdateCvSkill.ts
@@ -0,0 +1,26 @@
+import { gql,  useMutation } from "@apollo/client";
+import type { Cv, UpdateCvSkillInput } from "cv-graphql";
+
+export type UpdateCvSkillArgs = {
+  skill: UpdateCvSkillInput
+};
+
+export type UpdateCvSkillResult = {
+    data: Cv
+};
+
+export const UPDATE_CV_SKILL = gql`
+  mutation UpdateCvSkill($skill: UpdateCvSkillInput!) {
+    updateCvSkill(skill: $skill) {
+      skills {
+        name
+        mastery
+        categoryId
+      }
+    }
+  }
+`;
+
+export const useUpdateCvSkill = () => {
+  return useMutation<UpdateCvSkillResult, UpdateCvSkillArgs>(UPDATE_CV_SKILL)
+}
